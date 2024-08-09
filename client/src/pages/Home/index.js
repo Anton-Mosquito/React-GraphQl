50d257623e223f1028a59baf6b29d77358bd1275
anton.komarnytskyi <anton.komarnytskyi@masterofcode.com>
2024-08-09 06:18:06 +0000

    install apollo client and graphQl wrapt to provider main component add queries

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,6 +3,8 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import { MovieCard } from "../../components";
+import { useQuery } from "@apollo/client";
+import {MOVIES_QUERY} from './queries'
 
 const SelectedMovies = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,6 +17,11 @@ const SelectedMovies = styled(Paper)(({ theme }) => ({
 }));
 
 const Home = () => {
+  const { loading, error, data } = useQuery(MOVIES_QUERY);
+
+  if (error) {
+    return "Error";
+  }
   return (
     <Box sx={{ flexGrow: 1, marginTop: 2 }}>
       <Grid container spacing={2}>
@@ -24,11 +31,16 @@ const Home = () => {
         <Grid item xs={12} md={8}>
           <Paper>
             <Box sx={{ flexGrow: 1, padding: 1 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={6} md={4} lg={3}>
-                  <MovieCard />
+              {loading && "Loading..."}
+              {data && (
+                <Grid container spacing={2}>
+                  {data.movies.results.map((movie) => (
+                    <Grid key={movie.id} item xs={12} sm={6} md={4} lg={3}>
+                      <MovieCard movie={movie} />
+                    </Grid>
+                  ))}
                 </Grid>
-              </Grid>
+              )}
             </Box>
           </Paper>
         </Grid>
