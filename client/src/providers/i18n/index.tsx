1891447f156be9d11772de79425cdd56ca6de66f
Anton Komarnytskyi <anton0886@outlook.com>
2026-01-23 08:47:36 +0200

    chore(ts): convert i18n provider to TypeScript

---
+++ client/src/providers/i18n/index.tsx
@@ -0,0 +1,21 @@
+import React, { Fragment, ReactNode } from 'react';
+import { IntlProvider } from 'react-intl';
+import { flatten } from 'flat';
+
+import { LOCALES } from '../../const';
+import messages from '../../messages';
+
+type I18nProviderProps = {
+  children: ReactNode;
+  locale?: string;
+};
+
+const I18nProvider: React.FC<I18nProviderProps> = ({ children, locale = LOCALES.ENGLISH }) => (
+  <IntlProvider textComponent={Fragment} locale={locale} messages={flatten(messages[locale])}>
+    {children}
+  </IntlProvider>
+);
+
+I18nProvider.displayName = 'I18nProvider';
+
+export default I18nProvider;
