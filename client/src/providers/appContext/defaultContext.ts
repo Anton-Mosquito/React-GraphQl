f9005af3e980d9eafd851ccde0f7a52a157bc070
Anton Komarnytskyi <anton0886@outlook.com>
2026-01-23 08:46:34 +0200

    chore(ts): convert appContext to TypeScript (defaultContext + provider)

---
+++ client/src/providers/appContext/defaultContext.ts
@@ -0,0 +1,19 @@
+import { useSearchParams } from 'react-router-dom';
+import { LOCALES } from '../../const';
+import { STORAGE_KEY } from '../../const';
+import { getFromStorage } from '../../utils/localStorage';
+
+export type AppState = {
+  locale: string;
+};
+
+export const useDefaultContext = (): AppState => {
+  const [searchParams] = useSearchParams();
+
+  return {
+    locale:
+      (getFromStorage(STORAGE_KEY) as string | null) ||
+      (searchParams.get('locale') as string | null) ||
+      LOCALES.ENGLISH,
+  };
+};
